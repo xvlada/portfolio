cb97f09b51246803afb6b55ba0d77eccc1075048
vydidy <vydidy@gmail.com>
2023-10-30 16:43:24 +0100

    clean

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// // Set the target date to September 12, 2022
-// const targetDate = new Date("2022-09-12");
-    
-// // Function to calculate the number of days elapsed
-// function updateTimer() {
-//     const currentDate = new Date();
-//     const timeDifference = currentDate - targetDate;
-//     const daysElapsed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-//     // Display the number of days elapsed
-//     document.getElementById("timer").textContent = `Fun fact! I've been programming for ${daysElapsed} days!`;
-// }
-
-// // Call the updateTimer function initially to display the count
-// updateTimer();
-
-// // Update the count every second
-// setInterval(updateTimer, 1000);
-
- 
     // Set the target date and time to September 12, 2022, at 12:00 AM (midnight)
     const targetDate = new Date("2022-09-12T00:00:00");
 
@@ -25,11 +5,6 @@
     function updateTimer() {
         const currentDate = new Date();
         const timeDifference =  currentDate - targetDate;
-        if (timeDifference <= 0) {
-            document.getElementById("timer").textContent = "Regarding to the date on your computer, looks like I haven't started to learn programming yet. Update your date and time:)";
-            return;
-        }
-
         const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
@@ -42,6 +17,5 @@
     updateTimer();
 
     // Update the timer every second
-    
     setInterval(updateTimer, 1000); 
  
